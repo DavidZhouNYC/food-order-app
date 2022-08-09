0870196fa24a92043f48cb9df5324a13479267fe
David Zhou <11278771+DavidZhouNYC@users.noreply.github.com>
2022-08-08 22:54:33 -0400

    Add cart remove functionaiity

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -40,6 +40,33 @@ const cartReducer = (state, action) => {
         };
     }
     if (action.type === "REMOVE") {
+        const existingCartItemIndex = state.items.findIndex(
+            (item) => item.id === action.id
+        );
+
+        const existingItem = state.items[existingCartItemIndex];
+        const updatedTotalAmount = state.totalAmount - existingItem.price;
+        let updatedItems;
+
+        if (existingItem.amount === 1) {
+            // Remove item from cart if # of items is 1
+
+            updatedItems = state.items.filter((item) => item.id !== action.id);
+        } else {
+            // Decrease amount of items for id
+
+            const updatedItem = {
+                ...existingItem,
+                amount: existingItem.amount - 1,
+            };
+            updatedItems = [...state.items];
+            updatedItems[existingCartItemIndex] = updatedItem;
+        }
+
+        return {
+            items: updatedItems,
+            totalAmount: updatedTotalAmount,
+        };
     }
 
     return defaultCartState;
@@ -56,7 +83,7 @@ const CartProvider = (props) => {
     };
 
     const removeItemfromCartHandler = (id) => {
-        dispatchCartAction({ type: "REMOVVE", id: id });
+        dispatchCartAction({ type: "REMOVE", id: id });
     };
 
     const cartContext = {
